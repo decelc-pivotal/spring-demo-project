807125da57d721f1b71d2535cf59dba809c1b657
decelc-pivotal <cdecelles@pivotal.io>
2020-06-09 22:18:32 +0000

    update controller

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DemoController {
 
 	@GetMapping("/")
-		public String hello() {
-			return "hello world";
-		}
+	public String hello() {
+		return "hello world";
+	}
 }
